09605cb38c59c60581180388784591dd6f4d2ffa
Jarret Coyle <jarretcoyletoo@gmail.com>
2022-09-01 14:21:48 -0700

    page-fetcher completed

--- fetcher.js
+++ fetcher.js
@@ -1,2 +1,30 @@
-const args = process.argv(2);
-const net = require('net');
+const request = require("request");
+const process = require("process")
+const fs = require("fs");
+const localPath = process.argv[3];
+const url = process.argv[2];
+
+
+const fetcher = function(url, localPath) {
+  request(url,(err, response, body) => {
+    if (err) {
+      console.log(`There's an error!`, err)
+      return;
+    } else if (!err) {
+      fs.writeFile(localPath, body, (err) => {
+        if (err) {
+          console.log(`Error saving to local path`, localPath);
+          return;
+        } else {
+          console.log(`Downloaded/Saved to ${localPath}`)
+        };
+      });
+    };
+  });
+};
+
+if (url && localPath) {
+  fetcher(url, localPath)
+} else {
+  console.log(`Incorrect input!`)
+};
